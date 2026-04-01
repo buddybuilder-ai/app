9d6356b85c3be6fd186a0f8f6dfadd2b7d3f4bd6
GoodNightMare <nonthee.night@gmail.com>
2026-04-01 12:22:12 +0700

    fix ci

---
+++ types/global.d.ts
@@ -0,0 +1,16 @@
+// global.d.ts
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'a-scene': any;
+      'a-camera': any;
+      'a-entity': any;
+      'a-plane': any;
+      'a-assets': any;
+      'a-asset-item': any;
+      // เพิ่ม tag อื่นๆ ของ A-Frame ที่คุณใช้ที่นี่
+    }
+  }
+}
